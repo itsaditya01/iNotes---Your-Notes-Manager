037dc189024e5417ef31c489fde6689d41bec135
Kheval <khevalvaishnav@gmail.com>
2022-02-19 17:07:53 +0530

    second commit

--- src/components/SignUp/SignUp.component.jsx
+++ src/components/SignUp/SignUp.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./SignUp.style.css";
 import { FaUser, FaLock } from "react-icons/fa";
 import { BiRightArrowAlt } from "react-icons/bi";
+import { MdEmail } from "react-icons/md";
 
 function SignUp() {
   return (
@@ -25,11 +26,27 @@ function SignUp() {
               type="text"
               name="username"
               className="form__input"
-              placeholder="Username"
+              placeholder="Name"
               required
             />
           </div>
 
+          <div className="form__field">
+            <label for="login__password">
+              <svg className="icon">
+                <MdEmail />
+              </svg>
+              <span className="hidden">Email</span>
+            </label>
+            <input
+              id="login__password"
+              type="email"
+              name="email"
+              className="form__input"
+              placeholder="Email"
+              required
+            />
+          </div>
           <div className="form__field">
             <label for="login__password">
               <svg className="icon">
@@ -46,17 +63,32 @@ function SignUp() {
               required
             />
           </div>
-
+          <div className="form__field">
+            <label for="login__password">
+              <svg className="icon">
+                <FaLock />
+              </svg>
+              <span className="hidden">Confirm Password</span>
+            </label>
+            <input
+              id="login__password"
+              type="password"
+              name="password"
+              className="form__input"
+              placeholder="Confirm Password"
+              required
+            />
+          </div>
           <div className="form__field">
             <input type="submit" value="Sign In" />
           </div>
         </form>
 
         <p className="text--center text-white">
-          Not a member?{" "}
+          Already have an account?{" "}
           <a href="#" style={{ color: "#ea4c88" }}>
             {" "}
-            {"  Sign up now"}
+            {"  Login now"}
           </a>{" "}
           <svg className="icon" style={{ fontSize: "1.5em" }}>
             <BiRightArrowAlt style={{ color: "#ea4c88" }} />
